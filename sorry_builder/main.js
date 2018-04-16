943808774f794e98d3ec6d134e5a7c5e9f3d221c
ttc <wudong0338@gmail.com>
2018-04-16 23:11:28 +0800

    add render gif module

--- sorry_builder/main.js
+++ sorry_builder/main.js
@@ -5,25 +5,10 @@
     SRY_TPL = "assets/sorry/template.mp4",
     WJZ_TPL = "assets/wangjz/template.mp4",
     video = document.createElement('video'),
-    gif = new GIF({
-      workers: 2,
-      quality: 10
-    });
+    outputMaxWidth = 570;
 
-  gif.setOptions({
-    width: 570,
-    height: 320
-  });
-
-  // video.loop = "loop";
-  video.muted = true;
-
-  ctx.font = "20px Arial";
-  ctx.textAlign = "center";
-  ctx.fillStyle = "#fff";
-  ctx.strokeStyle = "#000";
-
-  let SRY_CONF = [
+  let W, H, _line,
+    SRY_LINES = [
       {start: 0.97, end: 1.50, line: "好啊"},
       {start: 3.11, end: 4.39, line: "就算你是一流工程师"},
       {start: 5.18, end: 7.26, line: "就算你出报告再完美"},
@@ -34,52 +19,65 @@
       {start: 18.08, end: 19.60, line: "以后天天叫他改报告"},
       {start: 19.60, end: 21.60, line: "天天改 天天改"}
     ],
-    WJZ_CONF = [];
+    WJZ_LINES = [];
 
-  let _line = undefined;
+  video.muted = true;
+
 
   video.addEventListener("timeupdate", () => {
     let _t = video.currentTime,
-      _lo = SRY_CONF.find((i) => { return i.start < _t && i.end > _t});
+      _lo = SRY_LINES.find((i) => { return i.start < _t && i.end > _t});
     _line = _lo && _lo.line
   });
 
   document.getElementById('render').addEventListener('click', () => {
-    init()
+    init();
   });
 
   const animate = () => {
-
+    ctx.clearRect(0, 0, W, H);
     ctx.drawImage(video, 0, 0);
-
-    if (_line) {
-      ctx.fillText(_line, 285, 290);
-      // ctx.strokeText(_line, 285, 290);
-    }
-    gif.addFrame(ctx);
-
+    _line && ctx.fillText(_line, 285, 290);
     return RAF(animate)
   };
 
-  gif.render();
-
-  gif.on('finished', (blob) => {
-    console.log(1, blob);
-    window.open(URL.createObjectURL(blob));
-  });
-
-
 
   let init = () => {
+    const gif = new GIF({
+      workers: 1,
+      quality: 10,
+      workerScript: './lib/gif.worker.js'
+    });
     video.src = SRY_TPL;
     video.onloadeddata = () => {
-      cvs.width = video.videoWidth;
-      cvs.height = video.videoHeight;
+      W = cvs.width = video.videoWidth;
+      H = cvs.height = video.videoHeight;
+      let duration = video.duration,
+        framesCount = duration * 1000 / 50,
+        rate = W > outputMaxWidth ? outputMaxWidth / W : 1;
+      gif.setOptions({
+        width: W * rate,
+        height: H * rate
+      });
+
+      ctx.scale(rate, rate);
+
+      ctx.font = "30px Arial";
+      ctx.textAlign = "center";
+      ctx.fillStyle = "#fff";
       video.play();
-      animate()
+      for (let i of new Array(framesCount)) {
+        gif.addFrame(ctx, {delay: 50})
+      }
+      gif.render();
     };
+    gif.on('finished', (blob) => {
+      window.open(URL.createObjectURL(blob));
+    })
   };
 
-  init()
+  animate();
+
+  init();
 
 })();
